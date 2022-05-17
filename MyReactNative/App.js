45aca5401784ce23c21a6fa373c5da728a8ed5a5
lolop123 <kris22dupstepgo@gmail.com>
2022-05-17 19:36:20 +0300

    first commit

---
+++ MyReactNative/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import Inputput from './functions/inp';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      
+      <Inputput/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
